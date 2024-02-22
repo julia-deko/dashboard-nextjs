1cb06b9af06d841256431ac1544f06d48b0c2616
Julia <julia.dzianisenka@gmail.com>
2024-02-22 12:36:15 +0100

    add static rendering

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,4 +1,17 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
+
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const res = await fetch('http://localhost:4000/tickets');
+
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id
+  }))
+}
 
 async function getTicket(id) {
   const res = await fetch(`http://localhost:4000/tickets/${id}`, {
@@ -7,8 +20,13 @@ async function getTicket(id) {
     }
   });
 
+  if(!res.ok) {
+    notFound();
+  }
+
   return res.json();
 }
+
 
 export default async function TicketsDetails({ params }) {
   const ticket = await getTicket(params.id);
