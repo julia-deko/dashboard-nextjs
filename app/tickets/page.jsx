5206ae0f8bdd2c314fe19db2ac5a420af90a7937
Julia <julia.dzianisenka@gmail.com>
2024-02-23 12:22:43 +0100

    add loading

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import TicketList from './TicketList'
+import Loading from '../loading'
 
 export default function Tickets() {
   return (
@@ -11,7 +12,9 @@ export default function Tickets() {
         </div>
       </nav>
 
-      <TicketList/>
+      <Suspense fallback={<Loading />}>
+        <TicketList/>
+      </Suspense>
     </main>
   )
 }
